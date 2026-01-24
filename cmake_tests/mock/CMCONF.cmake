82505494376f40a05c249dbcb81ead543c433de6
Jan Kubalek <janmkubalek@gmail.com>
2026-01-24 22:25:51 +0100

    cmake-tests init

---
+++ cmake_tests/mock/CMCONF.cmake
@@ -0,0 +1,15 @@
+##
+#
+# Mock CMCONF module for testing BA_PACKAGE_PREREQ_CMCONF_INIT
+#
+# This mock provides CMCONF_GET function that returns pre-configured test values.
+# Test values are set via TEST_CMCONF_<var_name> variables before including this mock.
+#
+
+FUNCTION(CMCONF_GET var_name)
+    IF(NOT DEFINED TEST_CMCONF_${var_name})
+        MESSAGE(FATAL_ERROR "Mock CMCONF_GET: TEST_CMCONF_${var_name} is not defined")
+    ENDIF()
+    SET(${var_name} "${TEST_CMCONF_${var_name}}" PARENT_SCOPE)
+ENDFUNCTION()
+
