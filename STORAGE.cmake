f003ee912891f653fa36d157df32a4752312dec2
Jan Kubalek <janmkubalek@gmail.com>
2026-02-17 16:04:33 +0100

    Update

--- STORAGE.cmake
+++ STORAGE.cmake
@@ -7,27 +7,27 @@ SET(BA_PACKAGE_CMCONF_USE TRUE
     CACHE BOOL
     "Switch on for CMCONF use; off for testing purposes. Do not alter this setting unless you know what you are doing."
 )
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/BA_PACKAGE_VARS.cmake")
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/BA_PACKAGE.cmake")
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/BA_PACKAGE_DEPS.cmake")
+
 IF(BA_PACKAGE_CMCONF_USE)
-    BA_PACKAGE_PREREQ_CMCONF_INIT(template revision git_archive_path_template)
+    BA_PACKAGE_PREREQ_CMCONF_INIT(template revision git_archive_path_template http_header template_args_uri_escape)
+    BA_PACKAGE_VARS_SET(REVISION             "${revision}")
+    BA_PACKAGE_VARS_SET(URI_TEMPLATE         "${template}")
+    BA_PACKAGE_VARS_SET(GIT_PATH_TEMPLATE    "${git_archive_path_template}")
+    BA_PACKAGE_VARS_SET(ESCAPE_TEMPLATE_ARGS "${template_args_uri_escape}")
+    BA_PACKAGE_VARS_SET(HTTP_HEADER          "${http_header}")
 ENDIF()
 
-IF(BA_PACKAGE_HTTP_HEADER)
+BA_PACKAGE_VARS_GET(HTTP_HEADER http_header)
+IF(http_header)
     IF(CMLIB_FILE_DOWNLOAD_HTTP_HEADER)
-        MESSAGE(WARNING "BA_PACKAGE_HTTP_HEADER is defined but CMLIB_FILE_DOWNLOAD_HTTP_HEADER is already set. Using BA_PACKAGE_HTTP_HEADER.")
+        MESSAGE(WARNING "BA_PACKAGE_HTTP_HEADER is defined but CMLIB_FILE_DOWNLOAD_HTTP_HEADER is already set. Using BA_PACKAGE HTTP_HEADER.")
     ENDIF()
-    SET(CMLIB_FILE_DOWNLOAD_HTTP_HEADER "${BA_PACKAGE_HTTP_HEADER}"
+    SET(CMLIB_FILE_DOWNLOAD_HTTP_HEADER "${http_header}"
         CACHE STRING
         "HTTP header set by Package Tracker to access the private repository"
         FORCE
     )
 ENDIF()
-
-
-
-INCLUDE("${CMAKE_CURRENT_LIST_DIR}/BA_PACKAGE_VARS.cmake")
-INCLUDE("${CMAKE_CURRENT_LIST_DIR}/BA_PACKAGE.cmake")
-INCLUDE("${CMAKE_CURRENT_LIST_DIR}/BA_PACKAGE_DEPS.cmake")
-
-BA_PACKAGE_VARS_SET(REVISION          "${revision}")
-BA_PACKAGE_VARS_SET(URI_TEMPLATE      "${template}")
-BA_PACKAGE_VARS_SET(GIT_PATH_TEMPLATE "${git_archive_path_template}")
